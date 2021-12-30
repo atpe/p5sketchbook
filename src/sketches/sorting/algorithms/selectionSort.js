6ca7107e671b45b5899f703b5f0d4f3c9d2ba182
Adam Evans <evans.adam.tp@gmail.com>
2021-12-30 16:13:07 +0000

    tidied up selection sort class

--- src/sketches/sorting/algorithms/selectionSort.js
+++ src/sketches/sorting/algorithms/selectionSort.js
@@ -3,37 +3,7 @@ import List from '../list'
 export default class SelectionSort extends List {
   constructor(width, height) {
     super(width, height)
-
-    this.status = 'SELECTING'
-
     this.i = 0
-  }
-
-  select() {
-    // Iterating over width
-    if (this.i < this.width) {
-      // Keeping or setting the minimum index
-      this.iMin = this.iMin || this.i
-      // Keeping or setting the comparison index
-      this.j = this.j || this.i + 1
-      // Iterating over width
-      if (this.j < this.width) {
-        // Setting minimum to lower value if found
-        if (this.items[this.j] < this.items[this.iMin]) {
-          this.iMin = this.j
-        }
-        // Increment comparison index
-        this.j++
-      } else {
-        this.j = undefined
-        // Swap current index with minimum index
-        if (this.iMin != this.i) {
-          [this.items[this.i], this.items[this.iMin]] = [this.items[this.iMin], this.items[this.i]]
-        }
-        this.i++
-        this.iMin = undefined
-      }
-    }
   }
 
   highlight(sketch) {
@@ -49,11 +19,31 @@ export default class SelectionSort extends List {
   }
 
   sort() {
-    switch (this.status) {
-      case 'SELECTING':
-        this.select()
-        break
-      default: break
+    // Iterating over width
+    if (this.i < this.width) {
+      // Keeping or setting the minimum index
+      this.iMin = this.iMin || this.i
+      // Keeping or setting the comparison index
+      this.j = this.j || this.i + 1
+      // Iterating over width
+      if (this.j < this.width) {
+        // Setting minimum to lower value if found
+        if (this.items[this.j] < this.items[this.iMin]) {
+          this.iMin = this.j
+        }
+        // Increment comparison index
+        this.j++
+      } else {
+        // Swap current index with minimum index
+        if (this.iMin != this.i) {
+          [this.items[this.i], this.items[this.iMin]] = [this.items[this.iMin], this.items[this.i]]
+        }
+        // Increment current index
+        this.i++
+        // Reset other variables
+        this.j = undefined
+        this.iMin = undefined
+      }
     }
   }
 }
